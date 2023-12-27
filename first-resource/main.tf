ca92aed147a5df0a81eab4554e69b6906abed09a
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2023-12-27 06:30:40 +0000

    Add further variable practice

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -1,11 +1,24 @@
-variable "vpcname" {
-  type    = string
-  default = "myvpc"
+variable "sshport" {
+  type    = number
+  default = 22
 }
 
-variable "cidr_block" {
-  type = string
-  default = "10.0.0.0/16"
+variable "enabled" {
+  type    = bool
+  default = true
+}
+
+variable "mylist" {
+  type    = list(string)
+  default = ["Value1", "Value2"]
+}
+
+variable "mymap" {
+  type = map
+  default = {
+    Key1 = "Value1"
+    Key2 = "Value2"
+  }
 }
 
 resource "aws_vpc" "myvpc" {
@@ -13,5 +26,9 @@ resource "aws_vpc" "myvpc" {
 
   tags = {
     Name = var.vpcname
+    SshPort = var.sshport
+    Enabled = var.enabled
+    ListElement = var.mylist[0]
+    MyMapElement = var.mymap.Key1
   }
 }
---
+++ first-resource/variables.tf
@@ -0,0 +1,9 @@
+variable "vpcname" {
+  type    = string
+  default = "myvpc"
+}
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
