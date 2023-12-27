8d911b7c14344d610d2834d9e10d9a4a9740ebce
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2023-12-27 04:57:00 +0000

    add linting corrections

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "eu-west-2"
 }
